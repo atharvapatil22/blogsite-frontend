612ae3728e46c68389122eada2a2e1624c99d0ac
atharvapatil22 <atharva.22.patil@gmail.com>
2022-03-30 00:39:52 +0530

    Integrated Draft editor

--- src/Screens/WriteBlog/WriteBlog.jsx
+++ src/Screens/WriteBlog/WriteBlog.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { DraftailEditor } from "draftail";
+import { EditorState } from "draft-js";
+import createInlineToolbarPlugin from "draft-js-inline-toolbar-plugin";
+import createSideToolbarPlugin from "draft-js-side-toolbar-plugin";
+
 import "./WriteBlog.css";
+import "draft-js/dist/Draft.css";
+import "draftail/dist/draftail.css";
+import "draft-js-inline-toolbar-plugin/lib/plugin.css";
+import "draft-js-side-toolbar-plugin/lib/plugin.css";
+
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
+const sideToolbarPlugin = createSideToolbarPlugin();
+const { SideToolbar } = sideToolbarPlugin;
+const plugins = [inlineToolbarPlugin, sideToolbarPlugin];
 
 function WriteBlog() {
-  return <div>WriteBlog</div>;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  return (
+    <div className="write-blog-container">
+      <DraftailEditor
+        editorState={editorState}
+        onChange={(newState) => setEditorState(newState)}
+        placeholder="Start writing your blog..."
+        plugins={plugins}
+      />
+      <InlineToolbar />
+      <SideToolbar />
+    </div>
+  );
 }
 
 export default WriteBlog;
