df9253a2f71b1b121be2e45a35fb3546b062802e
atharvapatil22 <atharva.22.patil@gmail.com>
2022-04-06 23:15:54 +0530

    Added Sidebar

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,9 +1,15 @@
 import "./Home.css";
 
 import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 
 function Home() {
-  return <div>Home Screen</div>;
+  return (
+    <div className="home-container">
+      <div className="home-body">Home Screen</div>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default Home;
--- src/Screens/Profile/Profile.jsx
+++ src/Screens/Profile/Profile.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 import "./Profile.css";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="profile-container">
+      <div className="profile-body">Profile Screen</div>
+      <Sidebar />
+    </div>
+  );
 }
 
 export default Profile;
