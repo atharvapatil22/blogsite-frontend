13295b9a81276a0469d72be63ab7a59e7e4b0f39
atharvapatil22 <atharva.22.patil@gmail.com>
2022-04-08 20:50:02 +0530

    created custom modal

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,30 @@
+import "./Modal.css";
+import React, { useEffect, useRef } from "react";
+import { GrClose } from "react-icons/gr";
+
+const Modal = ({ modalStyle, children, show, onClose, backdropStyle }) => {
+  const modalRef = useRef(null);
+  useEffect(() => {
+    if (show) {
+      modalRef.current.classList.add("visible");
+    } else {
+      modalRef.current.classList.remove("visible");
+    }
+  }, [show]);
+  return (
+    <React.Fragment>
+      <div ref={modalRef} style={backdropStyle} className="modal-wrap">
+        <div style={modalStyle} className="modal">
+          <div className="btn-container">
+            <button onClick={onClose} className={"close-btn"}>
+              <GrClose fontSize={"1.6em"} />
+            </button>
+          </div>
+          {children}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
