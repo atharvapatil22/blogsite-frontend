2ae50216495558f2e44effb221e55489541cfd99
atharvapatil22 <atharva.22.patil@gmail.com>
2022-06-03 20:29:42 +0530

    Created landing page

--- src/Screens/LandingPage/LandingPage.jsx
+++ src/Screens/LandingPage/LandingPage.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import "./LandingPage.css";
+import "../../assets/logo.png";
 
 function LandingPage() {
   return (
     <div className="landing-page">
-      <div className="landing-nav">
-        <p>Blogomo</p>
-        <div>
-          <button id="login-btn" type="button" onClick={() => {}}>
-            Login
-          </button>
-          <button id="get-started-btn" type="button" onClick={() => {}}>
-            Get Started
-          </button>
+      <div className="banner">
+        <div className="landing-main">
+          <div id="box1"></div>
+          <div id="box2">
+            <div id="text">Stay curious.</div>
+          </div>
+          <div id="box2a">
+            <div id="sub-text">
+              Discover stories, thinking, and expertise from writers on any
+              topic.
+            </div>
+          </div>
+          <div id="box3">
+            <div id="nav-container">
+              <div id="logo">
+                <img src={require("../../assets/logo.png")} />
+              </div>
+              <div id="menu">
+                <ul>
+                  <li>
+                    <button type="button">About</button>
+                  </li>
+                  <li>
+                    <button type="button">Login</button>
+                  </li>
+                  <li>
+                    <button type="button">Get Started</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="banner">Banner</div>
       <div className="mini-home">Mini Home</div>
     </div>
   );
