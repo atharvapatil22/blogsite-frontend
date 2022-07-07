34eeb5d36cf9e24d76f2c9a0b68f2ae6debc69c7
atharvapatil22 <atharva.22.patil@gmail.com>
2022-07-08 02:00:13 +0530

    added top navbar for mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,59 +17,69 @@ function Navbar() {
   )
     return null;
   return (
-    <header className={styles.navbar_container}>
-      <nav className={styles.navbar_body}>
-        <div className={`${styles.brand_logo} ${styles.hide_for_mobile}`}>
-          <Link to={"/home"}>
-            <div className={styles.home_link}>
-              <img src={require("../../assets/cogito_logo.png")} />
-            </div>
-          </Link>
-        </div>
+    <>
+      <div className={`${styles.nav_top} ${styles.show_for_mobile}`}>
+        <Link to={"/home"}>
+          <div className={styles.home_link_mobile}>
+            <img src={require("../../assets/cogito_logo.png")} />
+          </div>
+        </Link>
+        <div></div>
+      </div>
+      <header className={styles.navbar_container}>
+        <nav className={styles.navbar_body}>
+          <div className={`${styles.brand_logo} ${styles.hide_for_mobile}`}>
+            <Link to={"/home"}>
+              <div className={styles.home_link}>
+                <img src={require("../../assets/cogito_logo.png")} />
+              </div>
+            </Link>
+          </div>
 
-        <div className={styles.navbar_menu}>
-          <ul>
-            <li>
-              <Link to={"/home"}>
-                <div className={styles.navbar_link}>
-                  <BiHomeAlt fontSize={"2em"} />
-                </div>
-              </Link>
-            </li>
-            <li>
-              <Link to={"/write-blog"}>
-                <div className={styles.navbar_link}>
-                  <TiEdit fontSize={"2em"} />
-                </div>
-              </Link>
-            </li>
-            <li className={`${styles.profile_nav} ${styles.show_for_mobile}`}>
-              <Link to={"/profile"}>
-                <img
-                  src={
-                    userLoggedIn
-                      ? userLoggedIn.avatar
-                      : "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
-                  }
-                />
-              </Link>
-            </li>
-          </ul>
-        </div>
+          <div className={styles.navbar_menu}>
+            <ul>
+              <li>
+                <Link to={"/home"}>
+                  <div className={styles.navbar_link}>
+                    <BiHomeAlt fontSize={"2em"} />
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to={"/write-blog"}>
+                  <div className={styles.navbar_link}>
+                    <TiEdit fontSize={"2em"} />
+                  </div>
+                </Link>
+              </li>
+              <li className={`${styles.profile_nav} ${styles.show_for_mobile}`}>
+                <Link to={"/profile"}>
+                  <img
+                    src={
+                      userLoggedIn
+                        ? userLoggedIn.avatar
+                        : "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+                    }
+                  />
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className={styles.profile_nav}>
-          <Link to={"/profile"}>
-            <img
-              src={
-                userLoggedIn
-                  ? userLoggedIn.avatar
-                  : "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
-              }
-            />
-          </Link>
-        </div>
-      </nav>
-    </header>
+          <div className={styles.profile_nav}>
+            <Link to={"/profile"}>
+              <img
+                src={
+                  userLoggedIn
+                    ? userLoggedIn.avatar
+                    : "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+                }
+              />
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
 
